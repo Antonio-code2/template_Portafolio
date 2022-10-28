f478d002bcc03c3ebba5db1b5d109c6022b23e8a
DandevMS <xxdndkxx@gmail.com>
2022-10-27 22:54:56 -0500

    eventos de boton que hace scroll automatico hacia arriba agregado, evento de hacer aparecer el boton cuando se haga scroll hacia abajo agregado

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
 const navToggle = document.querySelector(".toggle");
 const navMenu = document.querySelector(".nav-menu");
+const button = document.querySelector("#up");
 
+/* evento para quitar o mostrar el menu  */
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu-visible")
 })
 
 
-const button = document.querySelector("#up");
+
+/* evento para hacer scroll al comienzo del documento  */
 button.addEventListener("click", () => {
     if (window.scrollY) {
-        window.scroll(0, 0);
+        window.scroll(0, 100);
       }
 });
 
 
+/* evento para mostrar el boton segun se haga scroll, si es mayor a 350 se aplica la clase, si no se remueve */
+
+window.onscroll = function(){scrollFuntion()}
+function scrollFuntion(){
+    if (document.documentElement.scrollTop > 350) {
+        button.classList.add('btn-show')
+    }else {
+        button.classList.remove('btn-show')
+    }
+}
