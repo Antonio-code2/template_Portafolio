3f8c5d359c62d756e19b6a7bc2719110da07ba4a
DandevMS <xxdndkxx@gmail.com>
2022-10-27 21:09:32 -0500

    main - funcionalidad boton scroll top, eventos implementados

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ const navMenu = document.querySelector(".nav-menu");
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu-visible")
 })
+
+
+const button = document.querySelector("#up");
+button.addEventListener("click", () => {
+    if (window.scrollY) {
+        window.scroll(0, 0);
+      }
+});
+
+
